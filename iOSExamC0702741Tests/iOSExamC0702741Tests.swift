a190824d74872c9ab4f9cf4fad6677187ed12591
Deepesh Mehta <deepeshhmehta@gmail.com>
2017-11-23 18:38:28 -0500

    Initial Commit

---
+++ iOSExamC0702741Tests/iOSExamC0702741Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSExamC0702741Tests.swift
+//  iOSExamC0702741Tests
+//
+//  Created by Deepesh Mehta on 2017-11-23.
+//  Copyright © 2017 Deepesh Mehta. All rights reserved.
+//
+
+import XCTest
+@testable import iOSExamC0702741
+
+class iOSExamC0702741Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
